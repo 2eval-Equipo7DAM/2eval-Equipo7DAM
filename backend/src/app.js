f0ee1776d602a87250b18e67313a2d2055fd498e
Willyknox <a28713@svalero.com>
2025-05-15 22:35:07 +0200

    changing to the routing tables for the DB

--- backend/src/app.js
+++ backend/src/app.js
@@ -13,7 +13,7 @@ app.use(express.json());
 const db = knex ({
     client: 'sqlite3',
     connection: {
-        filename: 'videogames.db'                    
+        filename: 'ardware.db'                    
     },
     useNullAsDefault: true
 });
@@ -27,106 +27,47 @@ const dbUser = knex ({
 });
 
 //CRUD
-//Mostrar todos los videojuegos disponibles
-app.get('/videogames', async(req, res) => {
-    const videogames = await db('videogames').select('*');
-    res.status(200).json(videogames);
+//Mostrar todas las GPUs disponibles
+app.get('/gpu', async (req, res) => {
+    const gpus = await db('GPU').select('*');
+    res.status(200).json(gpus);
 });
 
-//Mostrar videojuego determinado por nombre
-app.get('/videogames/:videogame', async (req, res) => {
-    const result = await db('videogames').select('*').where({name: req.params.videogame}).first();
-    
-    if (result === undefined){
-        res.status(404).json({
-            status: 'not-found',
-            message: 'Videogame not found'
-        });
-        return;
-    }
-
-    res.status(200).json(result);
+//Mostrar GPU por id en la URL
+app.get('/gpu/:gpuId', async (req, res) => {
+    const gpu = await db('GPU').select('*').where({ id: req.params.gpuId }).first();
+    res.status(200).json(gpu);
 });
 
-//Mostrar videojuego por id en la URL
-app.get('/videogames/:videogameId', async(req, res) => {
-    const videogame = await db('videogames').select('*').where({ id: req.params.videogameId }).first();
-    res.status(200).json(videogame); // ---> Se da el OK de la conexión a la BBDD
-});
-
-//Añadir un nuevo videojuego
-app.post('/videogames', async (req, res) => {
-
-    if (req.body.name === undefined || req.body.name === '') {
-        res.status(400).json({
-            status: 'bad-request',
-            message: 'Name of videogame is obligatory'
-        });
-        return;
-    }
-
-    if (req.body.type === undefined || req.body.type === '') {
-        res.status(400).json({
-            status: 'bad-request',
-            message: 'Type is necessary'
-        });
-        return;
-    }
-
-     if (req.body.year <= 0) {
-        res.status(400).json({
-            status: 'bad-request',
-            message: 'Year is necessary'
-        });
-        return;
-    }
-
-
-   const id = await db('videogames').insert({
-        name: req.body.name,
-        type:  req.body.type, // ---> todo esto mete los datos en la BBDD
-        year:  req.body.year
+//Añadir una nueva GPU
+app.post('/gpu', async function(req, res) {
+    await db('GPU').insert({
+        Brand: req.body.Brand,
+        Model: req.body.Model,
+        Price: req.body.Price,
+        Vram: req.body.Vram
     });
-
-    const newVideogame = await db('videogames').where({ id }).first();
-
-    res.status(201).json(newVideogame);  // ---> Aquí damos el OK al registrar el nuevo juego. No devuelve nada
+    res.status(201).json({ message: 'GPU created' });
 });
 
-
-
- //modificar un videojuego existente por ID
- app.put('/videogames/:videogameId', async (req, res) => {
-    const updated = await db('videogames').update({
-        name: req.body.name,
-        type: req.body.type,
-        year: req.body.year
-    }).where({id: req.params.videogameId});
-    if (updated) {
-        const updatedGame = await db('videogames').where({ id }).first();
-        res.status(200).json(updatedGame);
-    } else {
-        res.status(404).json({ error: 'Videogame not found' });
-    }
+//Modificar una GPU existente por ID
+app.put('/gpu/:gpuId', async (req, res) => {
+    await db('GPU').update({
+        Brand: req.body.Brand,
+        Model: req.body.Model,
+        Price: req.body.Price,
+        Vram: req.body.Vram
+    }).where({id: req.params.gpuId});
+    res.status(204).json({});
 });
 
-
-
-//borrar videojuego que existe
-app.delete('/videogames/:videogameId', async(req, res) => {
-    const deleted = await db('videogames').del().where({ id: req.params.videogameId });
-    if (deleted) {
-            res.status(200).json({ message: 'Videogame deleted' });
-        } else {
-            res.status(404).json({ error: 'Videogame not found' });
-        }
+//Borrar una GPU existente
+app.delete('/gpu/:gpuId', async (req, res) => {
+    await db('GPU').del().where({ id: req.params.gpuId });
+    res.status(204).json({});
 });
-
-
 
 //-----------------------------------------------------------------------------//
-
-
 
 //Mostrar todos los usuarios disponibles
 app.get('/users', async(req, res) => {
