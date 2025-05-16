79e8c09e89b7884a19493ba41ee2d127a703072e
DESKTOP-481791G\S2-PC00 <licencias@svalero.com>
2025-05-16 13:15:15 +0200

    Added integration tests

--- backend/src/test/integration/videogames.test.js
+++ backend/src/test/integration/videogames.test.js
@@ -8,78 +8,85 @@ chai.use(chaiHttp);
 chai.should();
 
 
-describe('GET /videogames', () => {
-    it('should return all videogames', (done) => { 
+describe('GET /gpu', () => {
+    it('should return all gpus', (done) => { 
         chai.request(app)
-            .get('/videogames')
+            .get('/gpu')
             .end((err, res) => {
                 expect(res).to.have.status(200); // Espera éxito
                 expect(res.body).to.be.an('array'); // El cuerpo de la respuesta es un array
-                expect(res.body.length).to.be.greaterThan(0); // Hay al menos un videojuego
+                expect(res.body.length).to.be.greaterThan(0); // Hay al menos una gpu
                 done();
             });
     });
 });
 
-describe('POST /videogames', () => {
-    it('should create a new videogame (success)', (done) =>  {
+describe('POST /gpu', () => {
+    it('should create a new gpu (success)', (done) =>  {
         chai.request(app)
-            .post('/videogames')
+            .post('/gpu')
             .send({ 
-                name: 'Test Game',
-                type: 'Action', 
-                year: 2024
+                brand: 'ASUS',
+                model: 'RTX 4090', 
+                vram: 24,
+                price: 1650
                 })
             .end((err, res) => {
                 expect(res).to.have.status(201); // Espera que la respuesta sea 201 (creado)
-                expect(res.body).to.have.property('name');
-                expect(res.body).to.have.property('type');
-                expect(res.body).to.have.property('year'); // El objeto devuelto tiene el nombre correcto
-                done();
-            });
-    });
-
-    it('should fail to create a videogame with missing fields', (done) => {
-        chai.request(app)
-            .post('/videogames')
-            .send({ 
-                name: '', 
-                type: 'Action' 
-            }) // Falta el campo year y el nombre está vacío
-            .end((err, res) => {
-                expect(res).to.have.status(400); // Espera error 400 (petición incorrecta)
-                expect(res.body.status).to.equal('bad-request');
-                expect(res.body).to.have.property('Name and year must be filled'); // El cuerpo debe tener un mensaje de error
-                done();
-            });
-    });
-});
-
-describe('PUT /videogames/:id', () => {
-    it('should update a videogame (success)', (done) => {
-        chai.request(app)
-            .put('/videogames/34')
-            .send({ 
-                name: 'Updated Game', 
-                type: 'RPG', 
-                year: 2023 
-            })
-            .end((err, res) => {
-                expect(res).to.have.status(200); // Espera éxito
-                expect(res.body).to.have.property('name');
-                expect(res.body).to.have.property('type');
+                expect(res.body).to.have.property('brand');
+                expect(res.body).to.have.property('model');
+                expect(res.body).to.have.property('vram'); // El objeto devuelto tiene el nombre correcto
                 expect(res.body).to.have.property('year');
                 done();
             });
     });
 
-    it('should fail to update a non-existent videogame', (done) => {
+    it('should fail to create a gpu with missing fields', (done) => {
         chai.request(app)
-            .put('/videogames/9999') // ID que no existe
+            .post('/gpu')
             .send({ 
-                name: 'No Game', 
-                type: 'RPG', 
-                year: 2023 })
+                brand: '', 
+                model: 'RTX 4080',
+                vram: 16
+            }) // Falta el campo price y la brand está vacía
+            .end((err, res) => {
+                expect(res).to.have.status(400); // Espera error 400 (petición incorrecta)
+                expect(res.body.status).to.equal('bad-request');
+                expect(res.body).to.have.property('brand and price must be filled'); // El cuerpo debe tener un mensaje de error
+                done();
+            });
+    });
+});
+
+describe('PUT /gpu/:id', () => {
+    it('should update a gpu (success)', (done) => {
+        chai.request(app)
+            .put('/gpu/24')
+            .send({ 
+                brand: 'Updated GPU',
+                model: 'RTX UPDATED', 
+                vram: 16,
+                price: 800
+            })
+            .end((err, res) => {
+                expect(res).to.have.status(200); // Espera éxito
+                expect(res.body).to.have.property('brand');
+                expect(res.body).to.have.property('model');
+                expect(res.body).to.have.property('vram');
+                expect(res.body).to.have.property('price');
+                done();
+            });
+    });
+
+    it('should fail to update a non-existent gpu', (done) => {
+        chai.request(app)
+            .put('/gpu/9999') // ID que no existe
+            .send({ 
+                brand: 'NO-GPU',
+                model: 'NO-MODEL', 
+                vram: 0,
+                price: 0
+            })
             .end((err, res) => {
                 expect(res).to.have.status(400); // Espera error 404 (no encontrado)
                 expect(res.body.status).to.equal('bad-request');
@@ -89,10 +96,10 @@ describe('PUT /videogames/:id', () => {
     });
 });
 
-describe('DELETE /videogames/:id', () => {
-    it('should delete a videogame (success)', (done) => {
+describe('DELETE /gpu/:id', () => {
+    it('should delete a GPU (success)', (done) => {
         chai.request(app)
-            .delete('/videogames/33')
+            .delete('/gpu/24')
             .end((err, res) => {
                 expect(res).to.have.status(200); // Espera éxito
                 expect(res.body).to.have.property('message');
@@ -100,13 +107,13 @@ describe('DELETE /videogames/:id', () => {
             });
     });
 
-    it('should fail to delete a non-existent videogame', (done) => {
+    it('should fail to delete a non-existent GPU', (done) => {
         chai.request(app)
-            .delete('/videogames/9999') // ID que no existe
+            .delete('/gpu/9999') // ID que no existe
             .end((err, res) => {
-                expect(res).to.have.status(400); // Espera error 404
+                expect(res).to.have.status(404); // Espera error 404
                 expect(res.body.status).to.equal('bad-request');
-                expect(res.body).to.have.property('Videogame not found'); // El cuerpo debe tener un mensaje de error
+                expect(res.body).to.have.property('GPU not found'); // El cuerpo debe tener un mensaje de error
                 done();
             });
     });
