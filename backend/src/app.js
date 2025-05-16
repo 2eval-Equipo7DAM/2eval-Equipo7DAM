581abae8c0f8e9540692a75d52deb48f915873fd
DESKTOP-481791G\S2-PC00 <licencias@svalero.com>
2025-05-16 12:39:22 +0200

    Multilayer Complete with Multilayer architecture

--- backend/src/app.js
+++ backend/src/app.js
@@ -2,73 +2,13 @@
 const express = require('express');
 const cors = require('cors');
 const knex = require('knex');
-
+const gpu = require('./route/gpu');
 
 //Iniciamos la aplicación (Backend)
 const app = express();
 app.use(cors());
 app.use(express.json());
-
-//Iniciar la BBDD
-const db = knex ({
-    client: 'sqlite3',
-    connection: {
-        filename: 'hardware.db'                    
-    },
-    useNullAsDefault: true
-});
-
-const dbUser = knex ({
-    client: 'sqlite3',
-    connection: {
-        filename: 'users.db'                  
-    },
-    useNullAsDefault: true
-});
-
-//CRUD
-//Mostrar todas las GPUs disponibles
-app.get('/gpu', async (req, res) => {
-    const gpus = await db('GPU').select('*');
-    res.status(200).json(gpus);
-});
-
-//Mostrar GPU por id en la URL
-app.get('/gpu/:gpuId', async (req, res) => {
-    const gpu = await db('GPU').select('*').where({ id: req.params.gpuId }).first();
-    res.status(200).json(gpu);
-});
-
-//Añadir una nueva GPU
-app.post('/gpu', async function(req, res) {
-    await db('GPU').insert({
-        Brand: req.body.Brand,
-        Model: req.body.Model,
-        Price: req.body.Price,
-        Vram: req.body.Vram
-    });
-    res.status(201).json({ message: 'GPU created' });
-});
-
-//Modificar una GPU existente por ID
-app.put('/gpu/:gpuId', async (req, res) => {
-    await db('GPU').update({
-        Brand: req.body.Brand,
-        Model: req.body.Model,
-        Price: req.body.Price,
-        Vram: req.body.Vram
-    }).where({id: req.params.gpuId});
-    res.status(204).json({});
-});
-
-//Borrar una GPU existente
-app.delete('/gpu/:gpuId', async (req, res) => {
-    await db('GPU').del().where({ id: req.params.gpuId });
-    res.status(204).json({});
-});
-
-//-----------------------------------------------------------------------------//
-
+app.use('/', gpu);
 
 app.listen(8080, () => {
     console.log("El backend ha iniciado correctamente en el puerto 8080");
---
+++ backend/src/service/gpu.js
@@ -0,0 +1,52 @@
+const knex = require('knex');
+
+const db = knex ({
+    client: 'sqlite3',
+    connection: {
+        filename: 'hardware.db'                    
+    },
+    useNullAsDefault: true
+});
+
+
+const findGpu = (async () => {
+    const gpus = await db('gpu').select('*');
+
+    return gpus;
+});
+
+const findGpuByName = (async (name) => {
+  const result = await db('gpu').select('*').where({name: name}).first();
+
+  return result;
+});
+
+const findGpuById = (async (id) => {
+   const gpu = await db('gpu').select('*').where({ id }).first();
+   return gpu;
+});
+
+const registerGpu = (async (brand, model, vram, price) => { 
+   const [id] = await db('gpu').insert({ brand, model, vram, price });
+    return await db('gpu').where({ id }).first();
+});
+
+const modifyGpu = async (id, brand, model, vram, price ) => {
+    const updated = await db('gpu').where({ id }).update({ brand, model, vram, price });
+    return updated;
+};
+
+const removeGpu = async (id) => {
+    const deleted = await db('gpu').where({ id }).del();
+    return deleted;
+};
+
+
+module.exports = {
+    findGpu,
+    findGpuByName,
+    findGpuById,
+    registerGpu,
+    modifyGpu,
+    removeGpu
+};
